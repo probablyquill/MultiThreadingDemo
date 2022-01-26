af48d7a3fd588b9c65ab2f5d6f5dde693b44361e
Elijah <37300152+probablyelijah@users.noreply.github.com>
2022-01-26 12:10:02 -0500

    Add Completed Code

---
+++ Handler.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace BruteForceExample
+{
+    class Handler
+    {
+        static void Main(string[] args)
+        {
+            String response;
+            int charNum;
+            Boolean upperIncluded = false;
+            Boolean numbersIncluded = false;
+            Boolean specialIncluded = false;
+
+            Console.WriteLine("Threaded:\n\ta.) Yes\n\tb.) No");
+
+            Console.WriteLine("Number of Characters:");
+            try {
+                charNum = int.Parse(Console.ReadLine());
+            } catch {
+                //Bad input
+                charNum = 2;
+            }
+
+            Console.WriteLine("Include Upper Case:\n\ta.) Yes\n\tb.) No");
+            response = Console.ReadLine().ToUpper();
+            if (response == "A" || response =="Y") {
+                upperIncluded = true;
+                Console.WriteLine("Including Upper Case");
+            } else {
+                Console.WriteLine("Not Including Upper Case.");
+            }
+
+            Console.WriteLine("Include Numbers:\n\ta.) Yes\n\tb.) No");
+            response = Console.ReadLine();
+            if (response == "A" || response =="Y") {
+                numbersIncluded = true;
+                Console.WriteLine("Including Numbers");
+            } else {
+                Console.WriteLine("Not Including Numbers.");
+            }
+
+            Console.WriteLine("Include Special Characters:\n\ta.) Yes\n\tb.) No");
+            response = Console.ReadLine();
+            if (response == "A" || response =="Y") {
+                specialIncluded = true;
+                Console.WriteLine("Including Special Characters");
+            } else {
+                Console.WriteLine("Not Including Special Characters.");
+            }
+            Console.WriteLine("Running...\n");
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            SingleThreaded runner = new SingleThreaded(upperIncluded, numbersIncluded, specialIncluded, charNum);
+            String outputText = runner.run();
+
+            watch.Stop();
+            Console.WriteLine("The generation took " + watch.ElapsedMilliseconds + "ms to complete.");
+            //Console.WriteLine(outputText);
+        }
+    }
+}
---
+++ bruteforcesinglethread.cs
@@ -0,0 +1,63 @@
+using System;
+
+namespace BruteForceExample
+{
+    class SingleThreaded
+    {
+        Boolean uppercase = false;
+        Boolean useNumbers = false;
+        Boolean useSpecialChars = false;
+        String output = "";
+        int length = 0;
+        public SingleThreaded() {
+            this.uppercase = false;
+            this.useNumbers = false;
+            this.useSpecialChars = false;
+            this.length = 2;
+        }
+        public SingleThreaded(Boolean uppercase, Boolean numbers, Boolean specialChars, int length) {
+            this.uppercase = uppercase;
+            this.useNumbers = numbers;
+            this.useSpecialChars = specialChars;
+            this.length = length;
+        }
+        public String run()
+        {
+            String characters = "abcdefghijklmnopqrstuvwxyz";
+            String numbers = "1234567890";
+            String specialChars = "!@#$%^&*()<>_+-=";
+
+            if (this.uppercase == true) characters += characters.ToUpper();
+            if (this.useNumbers == true) characters += numbers;
+            if (this.useSpecialChars == true) characters += specialChars;
+
+            //for (int i = 0; i < characters.Length; i++) {
+              //  for (int j = 0; j < characters.Length; j++) {
+                //    this.output += characters[i];
+                  //  this.output += characters[j];
+                    //this.output += "\n";
+                //}
+            //}
+
+            generate(this.length, 0, "", 0, characters.Length, characters);
+            return this.output;
+        }
+
+        void generate(int length, int depth, String previous, int start, int end, String characters) {
+            String saved = previous;
+
+            if (depth < length - 1) {
+                for (int i = 0; i < characters.Length; i++) {
+                    if (depth == 0) Console.WriteLine(characters[i]);
+                    previous += characters[i];
+                    generate(length, depth + 1, previous, start, end, characters);
+                    previous = saved;
+                } 
+            } else {
+                for (int i = 0; i < characters.Length; i++) {
+                    this.output += previous + characters[i] + "\n";
+                }
+            }
+        }
+    }
+}
